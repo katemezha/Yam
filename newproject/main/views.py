0008917b26483f0ed7f7711ae9636f424391dfc5
Ekaterina <44982634+katemezha@users.noreply.github.com>
2021-09-29 00:01:05 +0300

    Initial commit

---
+++ newproject/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def catalog(request):
+    return render(request, 'main/catalog.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
